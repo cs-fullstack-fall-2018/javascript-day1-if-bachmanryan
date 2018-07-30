eb89d48384c2bd01788554204765ba21f5910c9b
Ryan Bachman <jacksprat455@gmail.com>
2018-07-30 14:24:22 -0500

    If_statements

---
+++ if_pt1.js
@@ -0,0 +1,10 @@
+var anyword = ("Fred");
+var anotherWord = ("Greg");
+
+if (anyword == anotherWord){
+    alert("That is correct");
+}
+else{
+    alert("That is not correct");
+}
+
---
+++ if_pt2.js
@@ -0,0 +1,5 @@
+var num1 = parseInt(prompt("Enter a number: "));
+
+if (num1 > 0){
+    alert 
+}
