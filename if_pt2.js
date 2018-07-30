c1e06bfc8601cdfc9309cc34718a596715a02e23
Ryan Bachman <jacksprat455@gmail.com>
2018-07-30 14:32:16 -0500

    If_statements

--- if_pt2.js
+++ if_pt2.js
@@ -1,5 +1,16 @@
 var num1 = parseInt(prompt("Enter a number: "));
 
 if (num1 > 0){
-    alert 
+    alert("It's positive")
 }
+
+else if(num1 < 0){
+    alert("Its negative")
+}
+else if(num1 == 0){
+    alert("Zero")
+}
+else{
+    alert(nope)
+}
+
---
+++ if_pt3.js
@@ -0,0 +1,13 @@
+var num1 = parseInt(prompt("Enter a number: "));
+if(num1 == 1){
+    alert("Correct")
+}
+else if(num1 == 2){
+    alert("Correct")
+}
+else if(num1 == 3){
+    alert("Correct")
+}
+else{
+    alert("Error")
+}
